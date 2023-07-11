6cb2cd465a375b3fa1178ec336d0c154eb9db0d2
LeeChingHeang <70454146+LeeChingHeang@users.noreply.github.com>
2023-07-11 13:39:33 +0700

    add creat function

--- library.h
+++ library.h
@@ -1,4 +1,4 @@
-int create(int *arr, int size, int target);
+int create(int *arr, int size);
 int search(int *arr, int size, int target);
 int edit(int *arr, int size, int target);
 int del(int *arr, int size, int target);
